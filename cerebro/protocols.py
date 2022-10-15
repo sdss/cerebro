84e99b718311ba557e485e873bb3cf7762a26b27
José Sánchez-Gallego <gallegoj@uw.edu>
2022-10-15 18:46:20 +0000

    Add ActorClientSource

---
+++ cerebro/protocols.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2022-10-15
+# @Filename: protocols.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+import asyncio
+import random
+
+
+__all__ = ["ReconnectingTCPClientProtocol", "ClientProtocol"]
+
+
+class ReconnectingTCPClientProtocol(asyncio.Protocol):
+    """A reconnecting client modelled after Twisted ``ReconnectingClientFactory``.
+
+    Taken from https://bit.ly/3yn6MWa.
+    """
+
+    max_delay = 3600
+    initial_delay = 1.0
+    factor = 2.7182818284590451
+    jitter = 0.119626565582
+    max_retries = None
+
+    def __init__(self, *args, loop=None, **kwargs):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        self._loop = loop
+        self._args = args
+        self._kwargs = kwargs
+        self._retries = 0
+        self._delay = self.initial_delay
+        self._continue_trying = True
+        self._call_handle = None
+        self._connector = None
+
+    def connection_lost(self, exc):
+        if self._continue_trying:
+            self.retry()
+
+    def connection_failed(self, exc):
+        if self._continue_trying:
+            self.retry()
+
+    def retry(self):
+        if not self._continue_trying:
+            return
+
+        self._retries += 1
+        if self.max_retries is not None and (self._retries > self.max_retries):
+            return
+
+        self._delay = min(self._delay * self.factor, self.max_delay)
+        if self.jitter:
+            self._delay = random.normalvariate(self._delay, self._delay * self.jitter)
+        self._call_handle = self._loop.call_later(self._delay, self.connect)
+
+    def connect(self):
+        if self._connector is None:
+            self._connector = asyncio.create_task(self._connect())
+
+    async def _connect(self):
+        try:
+            await self._loop.create_connection(
+                lambda: self,
+                *self._args,
+                **self._kwargs,
+            )
+        except Exception as exc:
+            self._loop.call_soon(self.connection_failed, exc)
+        finally:
+            self._connector = None
+
+    def stop_trying(self):
+        if self._call_handle:
+            self._call_handle.cancel()
+            self._call_handle = None
+        self._continue_trying = False
+        if self._connector is not None:
+            self._connector.cancel()
+            self._connector = None
+
+
+class ClientProtocol(ReconnectingTCPClientProtocol):
+    """A reconnecting protocol."""
+
+    def __init__(self, on_received, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._on_received = on_received
+
+    def data_received(self, data):
+        self._loop.call_soon(self._on_received, data)
--- cerebro/sources/tron.py
+++ cerebro/sources/tron.py
@@ -18,7 +18,12 @@ import numpy
 
 from clu.legacy import TronConnection
 
+from cerebro.protocols import ClientProtocol
+from cerebro import log
 from .source import DataPoints, Source
+from clu.legacy.types.keys import Key, KeysDictionary
+from clu.legacy.types.messages import Keyword, Reply
+from clu.legacy.types.parser import ParseError, ActorReplyParser
 
 
 class TronSource(Source):
@@ -364,5 +369,246 @@ class TronTagsSource(Source):
             data=[{"measurement": actor, "tags": tags, "fields": {f"{name}": value}}],
             bucket=self.bucket,
         )
+
+        self.on_next(data_points)
+
+
+class ActorClientSource(Source):
+    """A data source that connects directly to an actor and issues periodic commands.
+
+    This source should be used to complement `.TronSource` when one wants a
+    command to be issued periodically without flooding the feed in Tron.
+
+    Parameters
+    ----------
+    name
+        The name of the data source.
+    actor
+        The name of the actor.
+    host
+        The host on which the actors is running.
+    port
+        The port on which the actor is running.
+    commands
+        A list of commands to issue to the actor on a timer.
+    interval
+        The interval, in seconds, between commands.
+    bucket
+        The bucket to write to. If not set it will use the default bucket.
+    tags
+        A dictionary of tags to be associated with all measurements.
+    store_broadcasts
+        Whether to store broadcast messages that may not be in response to a command.
+
+    """
+
+    source_type = "actor_client"
+    timeout = 60
+
+    def __init__(
+        self,
+        name: str,
+        actor: str,
+        host: str,
+        port: int,
+        commands: list[str],
+        interval: float = 60.0,
+        bucket: Optional[str] = None,
+        tags: Dict[str, Any] = {},
+        casts: dict[str, str] = {},
+        store_broadcasts: bool = False,
+    ):
+
+        super().__init__(name, bucket=bucket, tags=tags)
+
+        self.transport: asyncio.Transport | None = None
+        self.protocol: ClientProtocol | None = None
+
+        self.actor = actor
+        self.host = host
+        self.port = port
+
+        self.commands = commands
+        self._command_tasks: list[asyncio.Task] = []
+        self.interval = interval
+        self.casts = casts
+        self.store_broadcasts = store_broadcasts
+
+        self.buffer = b""
+
+        self.keyword_dict = KeysDictionary.load(actor)
+        self.rparser: Any = ActorReplyParser()
+
+    async def start(self, get_keys=True):
+        """Starts the connection to Tron.
+
+        Parameters
+        ----------
+        get_keys : bool
+            If `True`, gets all the keys in the models.
+        """
+
+        loop = asyncio.get_running_loop()
+        self.transport, self.protocol = await loop.create_connection(  # type: ignore
+            lambda: ClientProtocol(self._handle_reply),
+            self.host,
+            self.port,
+        )
+
+        for command in self.commands:
+            self._command_tasks.append(
+                asyncio.create_task(self.schedule_command(command))
+            )
+
+        self.running = True
+
+        return self
+
+    async def stop(self):
+        """Closes the connection."""
+
+        assert self.transport
+
+        for task in self._command_tasks:
+            task.cancel()
+            with suppress(asyncio.CancelledError):
+                await task
+        self._command_tasks = []
+
+        self.transport.close()
+
+        self.running = False
+        self.buffer = b""
+
+    def connected(self):
+        """Checks whether the client is connected."""
+
+        if self.transport is None:
+            return False
+
+        return not self.transport.is_closing()
+
+    async def schedule_command(self, command: str, interval: float | None = None):
+        """Schedules a command to be executed on an interval."""
+
+        interval = interval or self.interval
+
+        assert self.transport
+
+        while True:
+            self.transport.write(command.encode() + b"\n")
+            await asyncio.sleep(interval)
+
+    def _handle_reply(self, data: bytes):
+        """Processes a keyword received from the actor.
+
+        Mostly copied from CLU's ``TronConnection``.
+
+        """
+
+        self.buffer += data
+
+        lines = self.buffer.splitlines()
+        if not self.buffer.endswith(b"\n"):
+            self.buffer = lines.pop()
+        else:
+            self.buffer = b""
+
+        keys = []
+        for line in lines:
+            try:
+                # Do not strip here or that will cause parsing problems.
+                line = line.decode()
+                reply: Reply = self.rparser.parse(line)
+            except ParseError:
+                log.warning(f"{self.name}: failed parsing reply '{line.strip()}'.")
+                continue
+
+            for reply_key in reply.keywords:
+
+                key_name = reply_key.name.lower()
+                if key_name not in self.keyword_dict:
+                    log.warning(
+                        f"{self.name}: cannot parse unknown keyword "
+                        f"{self.actor}.{reply_key.name}.",
+                    )
+                    continue
+
+                # When parsed the values in reply_key are string. After consuming
+                # it with the Key, the values become typed values.
+                result = self.keyword_dict.keys[key_name].consume(reply_key)
+
+                if not result:
+                    log.warning(
+                        f"{self.name}: failed parsing keyword "
+                        f"{self.actor}.{reply_key.name}.",
+                    )
+                    continue
+
+                if reply.header.commandId == 0 and self.store_broadcasts is False:
+                    continue
+
+                keys.append(reply_key)
+
+        points = []
+
+        for key in keys:
+            name = key.name
+            actor = self.actor
+
+            ii = 0
+            for key_value in key.values:
+
+                if hasattr(key_value, "name") and key_value.name:
+                    key_name = f"_{key_value.name}"
+                elif len(key.values) == 1:
+                    key_name = ""
+                else:
+                    key_name = f"_{ii}"
+
+                tags = self.tags.copy()
+                if hasattr(key_value, "units"):
+                    tags.update({"units": key_value.units})
+
+                native = key_value.native
+                if isinstance(native, (list, tuple, numpy.ndarray)):
+                    if key_value.__class__.__name__ == "PVT":
+                        fields = {
+                            f"{name}{key_name}_P": native[0],
+                            f"{name}{key_name}_V": native[1],
+                            f"{name}{key_name}_T": native[2],
+                        }
+                    else:
+                        warnings.warn(
+                            f"Cannot parse {actor}.{name!r} of type {type(native)!r}.",
+                            UserWarning,
+                        )
+                        continue
+
+                else:
+                    parsed = native
+
+                    if f"{actor}.{name}{key_name}" in self.casts:
+                        cast = self.casts[f"{actor}.{name}{key_name}"]
+                        if cast == "int":
+                            parsed = int(native)
+                        elif cast == "float":
+                            parsed = float(native)
+                        elif cast == "bool":
+                            parsed = bool(native)
+
+                    fields = {f"{name}{key_name}": parsed}
+
+                points.append(
+                    {
+                        "measurement": actor,
+                        "tags": self.tags,
+                        "fields": fields,
+                    }
+                )
+
+                ii += 1
+
+        data_points = DataPoints(data=points, bucket=self.bucket)
 
         self.on_next(data_points)
