9ec428d3a1a9d4de1d623ff6edf2a445b247a762
José Sánchez-Gallego <gallegoj@uw.edu>
2022-10-16 07:57:02 +0000

    Add a unique identifier for all the points being added together

--- cerebro/cerebro.py
+++ cerebro/cerebro.py
@@ -410,6 +410,8 @@ class Cerebro(Subject, metaclass=MetaCerebro):
                 # Time is in nanoseconds since UNIX epoch.
                 point["time"] = meas_time
             point["tags"].update(self.tags)
+            # Add a unique identifier for all the points being added together.
+            point["tags"].update({"group_id": uuid.uuid4().hex[:8]})
 
         # Propagate to all the observers.
         Subject.on_next(self, data)
